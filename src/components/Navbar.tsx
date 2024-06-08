fe27425d7b4af6e63f088d331cc26c8357ef921d
Vishal <vishal2573003@gmail.com>
2024-06-08 21:41:40 +0530

    added tailwindcss

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,16 @@
+const Navbar = () => {
+  return (
+    <div>
+          <h1 className="text-2xl underline">
+      Hello world!
+    </h1>
+      <ul className="text-pink-800"> 
+        <li>home</li>
+        <li>home</li>
+        <li>home</li>
+      </ul>
+    </div>
+  )
+}
+
+export default Navbar
